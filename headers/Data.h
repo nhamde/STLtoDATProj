ad1894b37fd02bd8ba4a976137c98741f9e78db0
nhamde <narayan.hamde@cctech.co.in>
2024-10-07 20:56:11 +0530

    Add files via upload

---
+++ headers/Data.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Triangle.h"
+#include <unordered_map>
+using namespace std;
+
+class Data {
+public:
+	vector<double> ddVec;
+	unordered_map<double, size_t> map;
+	vector<Triangle*>trs;
+	vector<string> rawData;
+public:
+	void saveData();
+	Data();
+	~Data();
+};
